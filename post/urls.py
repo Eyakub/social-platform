b0c04199bcf18179dfd890bdcba3e6b95f98de6c
Eyakub <eyakubsorkar@gmail.com>
2020-05-21 17:41:36 +0600

    post like unlick done

--- post/urls.py
+++ post/urls.py
@@ -4,5 +4,7 @@ from post import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('liked/<pk>/', views.liked, name='liked'),
+    path('unliked/<pk>/', views.unliked, name='unliked'),
 ]
--- post/views.py
+++ post/views.py
@@ -13,6 +13,8 @@ from login.models import UserProfile, Follow
 def home(request):
     following_list = Follow.objects.filter(follower=request.user)
     posts = Post.objects.filter(author__in=following_list.values_list('following'))
+    liked_post = Like.objects.filter(user=request.user)
+    liked_post_list = liked_post.values_list('post', flat=True) #try printing without flat, combining into a LIST
     if request.method == 'GET':
         search = request.GET.get('search', '')
         result = User.objects.filter(username__icontains=search)
@@ -21,6 +23,7 @@ def home(request):
         'search': search,
         'result': result,
         'posts': posts,
+        'liked_post_list': liked_post_list
     }
     return render(request, 'posts/home.html', context=context)
 
@@ -32,4 +35,13 @@ def liked(request, pk):
     if not already_liked:
         liked_post = Like(post=post, user=request.user)
         liked_post.save()
-    return HttpResponseRedirect(reverse(''))
+    return HttpResponseRedirect(reverse('home'))
+
+
+@login_required
+def unliked(request, pk):
+    print('inside unliked')
+    post = Post.objects.get(pk=pk)
+    already_liked = Like.objects.filter(post=post, user=request.user)
+    already_liked.delete()
+    return HttpResponseRedirect(reverse('home'))
